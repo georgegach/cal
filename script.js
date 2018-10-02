022a1b8754b155dedca7e0a6327a56e3057330ed
George Gach <george@gach.space>
2018-10-02 16:08:49 -0400

    init

---
+++ script.js
@@ -0,0 +1,158 @@
+
+var monthsList = [
+    {},
+
+    {
+        name: 'Jan',
+        color: "#B2EBF2"
+    },
+
+    {
+        name: 'Feb',
+        color: "#80CBC4"
+    },
+    
+    {
+        name: 'Mar',
+        color: "#A5D6A7"
+    },
+
+    {
+        name: 'Apr',
+        color: "#C5E1A5"
+    },
+    
+    {
+        name: 'May',
+        color: "#E6EE9C"
+    },
+
+    {
+        name: 'June',
+        color: "#FFE082"
+    },
+
+    {
+        name: 'July',
+        color: "#FFAB91"
+    },
+
+    {
+        name: 'Aug',
+        color: "#EF9A9A"
+    },
+
+    {
+        name: 'Sep',
+        color: "#B39DDB"
+    },
+
+    {
+        name: 'Oct',
+        color: "#9FA8DA"
+    },
+
+    {
+        name: 'Nov',
+        color: "#64B5F6"
+    },
+
+    {
+        name: 'Dec',
+        color: "#81D4FA"
+    }
+]
+
+window.onload = function() {
+    var container = document.querySelector("#container")
+
+    var start = getParameterByName('start') || new Date().toISOString().slice(0,10)
+    var n = getParameterByName('n') || 20
+    var days = datesFrom(start, n)
+    var calEl = elementFrom(generateCalendar(days))
+    console.log(days)
+
+
+    container.appendChild(calEl)
+}
+
+
+
+function generateCalendar(days) {
+    
+    var global = `<div id='cal'>`
+    var months = `<div class='months'>`
+    for (let i = 1; i < monthsList.length; i++){
+        months += `<span class='month' style='background-color:${monthsList[i].color}'>${monthsList[i].name}</span>`
+    }
+    months += `</div>`
+
+
+    var quints = Math.ceil(days.length / 70)
+    var labels = `<span class='label'>Mon</span><span class='label'>Tue</span><span class='label'>Wed</span><span class='label'>Thu</span><span class='label'>Fri</span><span class='label' >Sat</span><span class='label' style='font-weight:bold'>Sun</span>`
+    labels += labels
+    for (let i = 0; i < quints; i++){
+        
+
+        global += `<div class='quint'>`
+        global += `<div class='labels'>${labels}</div>`
+
+        
+        for (let j = 0; j < 5; j++){
+            global += `<div class='week'>`
+            
+            qdays = days.slice(i*70 + j*14, (i*70) + (j+1)*14)
+            for (let j = 0; j < qdays.length; j++){
+                global += `<div class='day' style='background-color:${monthsList[qdays[j].m].color}'><span>${qdays[j].d}</span></div>`
+            }
+
+            global += `</div>`
+        }
+        if (i %2 == 1) {
+            global += months
+        }
+        global += `</div>`
+    }
+
+    return global
+}
+
+
+function elementFrom(text) {
+    var wrapper = document.createElement('div');
+    wrapper.innerHTML = text;
+    return wrapper.firstElementChild;
+}
+
+function datesFrom(start, n) {
+    var start = new Date(start)
+    var days = n*7*2 // doubleweek days
+    var list = []
+
+    // find monday
+    while (start.getDay() != 0){
+        start.setDate(start.getDate() - 1)
+    }
+
+    for (let i = 0; i < days; i++){ 
+        var strDate = start.toISOString().slice(0,10)
+        list.push({
+            d : parseInt(strDate.split('-')[2]),
+            m : parseInt(strDate.split('-')[1]),
+            y : parseInt(strDate.split('-')[0]),
+        })
+        start.setDate(start.getDate()+1)
+    }
+
+    return list
+}
+
+function getParameterByName(name, url) {
+    if (!url) url = window.location.href;
+    name = name.replace(/[\[\]]/g, "\\$&");
+    var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+        results = regex.exec(url);
+    if (!results) return null;
+    if (!results[2]) return '';
+    return decodeURIComponent(results[2].replace(/\+/g, " "));
+}
